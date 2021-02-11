458a72e717399b6fd3b59dec979de8ff75ab4619
Jenish Rudani <jrudani1999@gmail.com>
2021-02-11 21:40:31 +0530

    formated blink.c

--- blink/blink.c
+++ blink/blink.c
@@ -3,14 +3,15 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-
 #include "pico/stdlib.h"
 
-int main() {
+int main()
+{
     const uint LED_PIN = 25;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
+    while (true)
+    {
         gpio_put(LED_PIN, 1);
         sleep_ms(250);
         gpio_put(LED_PIN, 0);
